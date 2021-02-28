a328075eb4297dc8dfaded98ae0cee5bf9e11e32
JPB <jbanas9124@gmail.com>
2021-02-28 14:17:06 -0600

    basic html sections started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,54 @@
+//different page identifier variables
+//home page
+var homeSectionsEl = document.querySelector(".score-and-timer, .quiz-home");
+
+//quiz start for eventlistner
+var startEl = document.querySelector("#start-quiz");
+
+//questions page
+var questionsEl = document.querySelector(".quiz-question");
+
+//finish questions/ give score page
+var endEl = document.querySelector(".quiz-final-score");
+
+//high scores page
+var highScoreEl = document.querySelector(".high-scores");
+
+
+//console.log(homeSectionsEl, questionsEl, endEl, highScoreEl);
+
+//on page load only show the sections with "score-and-timer" & "quiz-home" class
+var homePageDisplay = function() {
+  if (questionsEl || endEl || highScoreEl) {
+    questionsEl.style.display = "none";
+    endEl.style.display = "none";
+    highScoreEl.style.display = "none";
+  }
+};
+
+window.onload = homePageDisplay();
+
+//quiz start main function
+var quizStart = function(event) {
+  
+}
+
+
+//when clicking 'start quiz' button call the function
+//for quiz questions display 
+startEl.addEventListener("click", quizStart);
+
+
+
+
+
+
+
+
+
+
+
+
 //when the 'start quiz' button is pressed a timer starts
 // a question appears on the page with four optional answer buttons
 // a correct answer will move on to the next page and display 'correct' in italicized gray letters
