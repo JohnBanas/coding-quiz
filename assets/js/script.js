5d01dbf9f12ce0e651d59d1da2467a69e9564a8e
JPB <jbanas9124@gmail.com>
2021-02-27 21:37:54 -0600

    psuedo code for application

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//when the 'start quiz' button is pressed a timer starts
+// a question appears on the page with four optional answer buttons
+// a correct answer will move on to the next page and display 'correct' in italicized gray letters
+// a wrong answer will subtract 10 seconds from the timer but still moves on the next question and alert wrong
+//after the questions have all been answered you are taken to an all done screen 
+//at the all done screen your left over time is displayed as your score
+//you are prompted to enter your initials
+// if you enter your initials and hit the submit button or enter you go to the high scores page
+// the high scores page has saved numbered positions for the high scores 
+// you also have two buttons 
+//one button labled go back will take you to the start quiz screen
+//the second button will clear the high scores list to start over
