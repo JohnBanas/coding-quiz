82b77ec496934e58d45ce3421f998c4400b19971
JPB <jbanas9124@gmail.com>
2021-03-08 11:52:41 -0600

    Refactor and condensing Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,229 @@
+//body
+var bodyEl = document.querySelector(".page-content")
 //link to <header class="score-link-timer" id="score-link-timer"> High Score Link & Timer
-var scoreLinkAndTimerEl = document.querySelector(".score-link-timer");
+var scoreLinkEl = document.querySelector(".high-score-link");
+
+var timerEl = document.querySelector(".timer");
 
 //link to <section class="start-game" id="start-game"> for start/home page
 var startGameEl = document.querySelector(".start-game");
 
-//Link to <section class="questions" id="questions"> for... you guessed it ... questions
-var questionsEl = document.querySelector(".questions");
+var startGameBtnEl = document.querySelector("#start-btn");
+
+
+//Link  for... you guessed it ... questions
+
+//all question sections
+var questionsEl = document.querySelector(".question-contain");
+
+//individual question & answers
+var questionOneEl = document.querySelector("#section-question-one");
+var questionTwoEl = document.querySelector("#section-question-two");
+var questionThreeEl = document.querySelector("#section-question-three");
+var questionFourEl = document.querySelector("#section-question-four");
+var questionFiveEl = document.querySelector("#section-question-five");
+
+var questionArray = [questionOneEl, questionTwoEl,questionThreeEl,questionFourEl,questionFiveEl];
+
+//answers ? one
+//all
+var btnsOne = document.querySelector(".btn-one");
+
+//individual
+var correctAnsOne = document.querySelector("#one-btn-one");
+var oneWrongAnsOne = document.querySelector("#one-btn-two");
+var oneWrongAnsTwo = document.querySelector("#one-btn-three");
+var oneWrongAnsThree = document.querySelector("#one-btn-one");
+
+//answers ? two
+//all
+var btnsTwo = document.querySelector(".btn-two");
+
+var correctAnsTwo = document.querySelector("#two-btn-three");
+var twoWrongAnsOne = document.querySelector("#two-btn-one");
+var twoWrongAnsTwo = document.querySelector("#two-btn-two");
+var twoWrongAnsThree = document.querySelector("#two-btn-four");
+
+//answers ? 3
+//all
+var btnsThree = document.querySelector(".btn-three");
+
+var correctAnsThree = document.querySelector("#three-btn-four");
+var threeWrongAnsOne = document.querySelector("#three-btn-one");
+var threeWrongAnsTwo = document.querySelector("#three-btn-two");
+var threeWrongAnsThree = document.querySelector("#three-btn-three");
+
+//answers ? 4
+//all
+var btnsFour = document.querySelector(".btn-four");
+
+var correctAnsFour = document.querySelector("#four-btn-three");
+var fourWrongAnsOne = document.querySelector("#four-btn-one");
+var fourWrongAnsTwo = document.querySelector("#four-btn-two");
+var fourWrongAnsThree = document.querySelector("#four-btn-four");
+
+//answers ? 5
+//all
+var btnsFive = document.querySelector(".btn-five");
+
+var correctAnsFive = document.querySelector("#four-btn-three");
+var fiveWrongAnsOne = document.querySelector("#four-btn-one");
+var fiveWrongAnsTwo = document.querySelector("#four-btn-two");
+var fiveWrongAnsThree = document.querySelector("#four-btn-four");
 
 //link to <section class="finish-game" id="finish-game"> end game page
 var finishGameEl = document.querySelector(".finish-game");
+var finalScoreEl = document.querySelector(".final-score");
 
-//Link to <section class="high-scores" id="high-scores"> High Scores page
-var highScoreLinkEl = document.querySelector(".high-scores");
+var initialRetrieve = localStorage.getItem("initials");
 
-//array of all sections
-var pageContent = [scoreLinkAndTimerEl, startGameEl, questionsEl, finishGameEl, highScoreLinkEl];
+var submitBtnEl = document.querySelector(".submit-btn");
 
-//array of question array and answer array(s)
-var quiz = {
-  /** Question 1: quiz.question[0]
-   * Question 2: quiz.question[1]
-   * Question 3: quiz.question[2]
-   * Question 4: quiz.question[3] **/
-  question:["Commonly used data types DO NOT include:", 
-    "The condition of an if/else statement is enclosed with _______ .",
-    "Arrays in JavaScript can be used to store _______ .",
-    "String values must be enclosed within _______ when being assigned to variables.",
-    "A very useful tool used during development and debugging for printing content to the debugger is:"],
-  choices:[
-    //question one choices
-    ["Alerts", "Strings", "Booleans", "Numbers"],
-    //question two choices
-    ["Parenthesis", "Quotes", "Curly Brackets", "Square Brackets"],
-    //question three choices
-    ["Numbers and strings", "Other Arrays", "Booleans","All of the above"],
-    //question four choices
-    ["Commas", " Curly Brackets", "Quotes", "Parenthesis"],
-    //question five choices
-    ["JavaScript", "For Loops", "Terminal/Bash", "Console.log()"],
-  ],  
-  //answers in order 0-3 === question 0-3
-  answers:["Alerts", "Parenthesis","All of the above", "Quotes", "Console.log"],
-}; 
+var highScorePageEl = document.querySelector(".high-scores");
+var highScoreLinkEl = document.querySelector("#high-score-link");
+var addHighScoreHere = document.querySelector(".high-score-form");
+var goBackEl = document.querySelector("#go-back");
+var clearHighScoreEl = document.querySelector("#clear-scores")
+var scoreRankCounter = 0;
 
 
-
-//on load begin/quiz instruction
-var begin = function() {
-  for (var i = 0; i < pageContent.length; i++) 
-    if (pageContent[i] !== scoreLinkAndTimerEl && pageContent[i] !== startGameEl) {
-      pageContent[i].style.display = "none";
-    } 
-  scoreLinkAndTimerEl.innerHTML = '<span><a href="#high-scores">View High Scores</a></span>' + '<span class="timer">Timer:0</span>';
-  startGameEl.innerHTML ='<p>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your time by ten seconds!';
-  startGameEl.insertAdjacentHTML('beforeend','<button class="start btn">Start Game</button>');  
-}; 
+//initial display
 window.onload = function(){
-  begin();
-}
-
-var quizPage = function(){
-  for (var i = 0; i < pageContent.length; i++)
-  if (pageContent[i] !== questionsEl && pageContent[i] !== scoreLinkAndTimerEl) {
-    pageContent[i].style.display = "none";
-  } else {
-    pageContent[i].style.display = "block";
+  clearPage();
   }
-  questionsEl.innerHTML = '<p class="questions">' + quiz.question[0] + '</p>';
-  
-  for (var i = 0; i < quiz.choices[0].length; i ++)
-  console.log(quiz.choices[0][i]); 
 
-  questionsEl.insertAdjacentElement('beforeend',"div");
+function clearPage(){
+  startGameEl.style.display = "block";
+  startGameBtnEl.style.display = "block";
+  scoreLinkEl.style.display = "block";
+  timerEl.style.display = "block";
+  questionsEl.style.display = "none";
+  finishGameEl.style.display = "none";
+  highScorePageEl.style.display ="none";
 }
-startGameEl.addEventListener("click", quizPage);
-//quizPage();
+
+function initializeGame(){
+  startGameEl.style.display = "none";
+  startGameBtnEl.style.display = "none";
+  questionsEl.style.display = "block";
+  questionOneEl.style.display = "block";
+  questionTwoEl.style.display = "none";
+  questionThreeEl.style.display = "none";
+  questionFourEl.style.display ="none";
+  questionFiveEl.style.display = "none";
+}
+
+function firstQuestionRight(){
+  questionOneEl.style.display = "none";
+  questionTwoEl.style.display = "block";
+}
+
+function firstQuestion(){
+  questionOneEl.style.display = "none";
+  questionTwoEl.style.display = "block";
+  secondsLeft -= 10;
+}
+
+function secondQuestionRight(){
+  questionTwoEl.style.display = "none";
+  questionThreeEl.style.display = "block";
+}
+
+function secondQuestion(){
+  questionTwoEl.style.display = "none";
+  questionThreeEl.style.display = "block";
+  secondsLeft -= 10;
+}
+
+function thirdQuestionRight(){
+  questionThreeEl.style.display = "none";
+  questionFourEl.style.display = "block";
+}
+
+function thirdQuestion(){
+  questionThreeEl.style.display = "none";
+  questionFourEl.style.display = "block";
+  secondsLeft -= 10;
+}
+
+function fourthQuestionRight(){
+  questionFourEl.style.display = "none";
+  questionFiveEl.style.display = "block";
+}
+
+function fourthQuestion(){
+  questionFourEl.style.display = "none";
+  questionFiveEl.style.display = "block";
+  secondsLeft -= 10;
+}
+
+function finishGameRight(){
+  stop();
+  finalScoreEl.textContent = "Your final score is:" + secondsLeft;
+  questionFiveEl.style.display = "none";
+  finishGameEl.style.display = "block";
+  scoreLinkEl.style.display = "none";
+  timerEl.style.display = "none";
+}
+
+function finishGame(){
+  stop();
+  finalScoreEl.textContent = "Your final score is:" + secondsLeft;
+  questionFiveEl.style.display = "none";
+  finishGameEl.style.display = "block";
+  scoreLinkEl.style.display = "block";
+  timerEl.style.display = "block";
+  secondsLeft -= 10;
+}
+
+function highScores(){
+  timerEl.style.display = "none";
+  finishGameEl.style.display = "none";
+  startGameEl.style.display = "none";
+  startGameBtnEl.style.display = "none";
+  questionsEl.style.display = "none";
+  highScorePageEl.style.display = "block";
+}
+
+function storeInitials() {
+  var initialsPlace = document.getElementById("your-initials").value;
+  var highScoreListItem = document.createElement("li");
+  highScoreListItem.className = "high-score-initials";
+  highScoreListItem.setAttribute("high-score-rank", scoreRankCounter);
+  highScoreListItem.textContent = initialsPlace;
+  addHighScoreHere.appendChild(highScoreListItem);
+  scoreRankCounter++; 
+}
+
+function clear(event){
+  addHighScoreHere.innerHTML = '';
+}
 
 // var timerEl
 //variables to connect to page
-var timeEl = document.querySelector(".timer");
+
 
 //timer function for start game and wrong answers
 //timer set amount
-var secondsLeft = 10;
-  var timerInterval;
+var startTimer = 75;
+var secondsLeft = 75;
+var timerInterval;
 
 //the timing interval of seconds
-function start(){
-  timerInterval = setInterval(count, 1000);
+function count(){
+  timerEl.textContent = "Timer:" + secondsLeft;
+  secondsLeft-= 1;
+  timerInterval = setTimeout(count, 1000); 
+}
+
+function setTime(){
+  secondsLeft = 75;
+  count();
 }
 
 //stop the timer
 function stop(){
-  clearInterval(timerInterval);
-  sendMessage();
-}
-
-//countdown
-function count() {
-  secondsLeft--;
-  timeEl.textContent = "Timer:" + secondsLeft;
-  if(secondsLeft === 0) {
-    stop();
-  }
+  clearTimeout(timerInterval);
 }
 
 //wrong answer
@@ -111,36 +232,86 @@ function loseTime() {
   if(secondsLeft<=0) {
     stop();
   }
-  else  {
-  timeEl.textContent = "Timer:" + secondsLeft;
+}
+
+
+
+/** <!DOCTYPE html>
+<html>
+<body>
+
+<button onclick="startCount()">Start count!</button> = startGameBtnEl.addEventListener("click", setTime);
+<input type="text" id="txt"> = timerEl
+<button onclick="stopCount()">Stop count!</button> finishGame or finishGameRight
+
+<p>
+Click on the "Start count!" button above to start the timer. The input field will count forever, starting at 0. Click on the "Stop count!" button to stop the counting. Click on the "Start count!" button to start the timer again.
+</p>
+
+<script>
+var c = 0; = secondsLeft
+var t; timerInterval
+var timer_is_on = 0;
+
+function timedCount() {
+  document.getElementById("txt").value = c;
+  c = c + 1;
+  t = setTimeout(timedCount, 1000);
+}
+
+function startCount() {
+  if (!timer_is_on) {
+    timer_is_on = 1;
+    timedCount();
   }
 }
 
-//start the timer
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = "Timer:" + secondsLeft;
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-  }, 1000);
+function stopCount() {
+  clearTimeout(t);
+  timer_is_on = 0;
 }
+</script>
 
-//after stop send a message
-function sendMessage() {
-  if (secondsLeft === 0) {
-  timeEl.textContent = "Game Over";
-  } else {
-    timeEl.textContent = "Your Final Score is " + secondsLeft;
-  }
-}
+</body>
+</html> **/
+
+startGameBtnEl.addEventListener("click", setTime);
+startGameBtnEl.addEventListener("click", initializeGame);
+
+correctAnsOne.addEventListener("click", firstQuestionRight);
+oneWrongAnsOne.addEventListener("click", firstQuestion);
+oneWrongAnsTwo.addEventListener("click", firstQuestion);
+oneWrongAnsThree.addEventListener("click", firstQuestion);
+
+correctAnsTwo.addEventListener("click", secondQuestionRight);
+twoWrongAnsOne.addEventListener("click", secondQuestion);
+twoWrongAnsTwo.addEventListener("click", secondQuestion);
+twoWrongAnsThree.addEventListener("click", secondQuestion);
+
+correctAnsThree.addEventListener("click", thirdQuestionRight);
+threeWrongAnsOne.addEventListener("click", thirdQuestion);
+threeWrongAnsTwo.addEventListener("click", thirdQuestion);
+threeWrongAnsThree.addEventListener("click", thirdQuestion);
+
+correctAnsFour.addEventListener("click", fourthQuestionRight);
+fourWrongAnsOne.addEventListener("click", fourthQuestion);
+fourWrongAnsTwo.addEventListener("click", fourthQuestion);
+fourWrongAnsThree.addEventListener("click", fourthQuestion);
+
+correctAnsFive.addEventListener("click", finishGameRight);
+fiveWrongAnsOne.addEventListener("click", finishGame);
+fiveWrongAnsTwo.addEventListener("click", finishGame);
+fiveWrongAnsThree.addEventListener("click", finishGame);
+
+submitBtnEl.addEventListener("click", highScores);
+highScoreLinkEl.addEventListener("click", highScores);
+goBackEl.addEventListener("click", clearPage);
+
+clearHighScoreEl.addEventListener("click", clear);
 
 
 
-//clicking Start Quiz starts the timer
-//startGameEl.addEventListener("click", gameRun); 
+
 
 
 
@@ -163,4 +334,4 @@ function sendMessage() {
 // the high scores page has saved numbered positions for the high scores 
 // you also have two buttons 
 //one button labled go back will take you to the start quiz screen
-//the second button will clear the high scores list to start over*/
+//the second button will clear the high scores list to start over
